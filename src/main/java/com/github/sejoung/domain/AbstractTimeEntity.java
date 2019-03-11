89fd10e7561f81f9ebf917fec846523617894bbb
sejoung kim <sejoung@gmail.com>
2019-03-11 14:10:58 +0900

    수정

--- src/main/java/com/github/sejoung/domain/AbstractTimeEntity.java
+++ src/main/java/com/github/sejoung/domain/AbstractTimeEntity.java
@@ -6,19 +6,21 @@ import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-@Setter
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class AbstractTimeEntity {
 
+  @Setter(value = AccessLevel.PRIVATE)
   @Column(nullable = false)
   private LocalDateTime createDateTime;
 
+  @Setter(value = AccessLevel.PRIVATE)
   @Column(nullable = false)
   private LocalDateTime updateDateTime;
 
--- src/main/java/com/github/sejoung/domain/DomainTest.java
+++ src/main/java/com/github/sejoung/domain/DomainTest.java
@@ -5,7 +5,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
