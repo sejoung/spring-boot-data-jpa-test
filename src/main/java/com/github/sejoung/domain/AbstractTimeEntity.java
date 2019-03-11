519b1f9a8a091ab7f327142d6f5140c2e42e1445
sejoung kim <sejoung@gmail.com>
2019-03-11 17:51:44 +0900

    이게 조금더 마음에 드는듯

--- src/main/java/com/github/sejoung/domain/AbstractTimeEntity.java
+++ src/main/java/com/github/sejoung/domain/AbstractTimeEntity.java
@@ -3,22 +3,34 @@ package com.github.sejoung.domain;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import lombok.AccessLevel;
 import lombok.Getter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+import lombok.Setter;
 
 @Getter
 @MappedSuperclass
 public abstract class AbstractTimeEntity {
 
-  @CreationTimestamp
+  @Setter(value = AccessLevel.PRIVATE)
   @Column(nullable = false)
   private LocalDateTime createDateTime;
 
-  @UpdateTimestamp
+  @Setter(value = AccessLevel.PRIVATE)
   @Column(nullable = false)
   private LocalDateTime updateDateTime;
 
+  @PrePersist
+  private void prePersist() {
+    LocalDateTime now = LocalDateTime.now();
+    this.setCreateDateTime(now);
+    this.setUpdateDateTime(now);
+  }
 
-
+  @PreUpdate
+  private void preUpdate() {
+    LocalDateTime now = LocalDateTime.now();
+    this.setUpdateDateTime(now);
+  }
 }
