f7f16bdcd9d3abdeed03de7faba6a4d9186d069d
sejoung kim <sejoung@gmail.com>
2019-03-12 16:12:14 +0900

    테스트 코드 작성

--- src/test/java/com/github/sejoung/repositories/TestRepositoryDomainDomainTest.java
+++ src/test/java/com/github/sejoung/repositories/TestRepositoryDomainDomainTest.java
@@ -1,37 +1,51 @@
 package com.github.sejoung.repositories;
 
+
 import com.github.sejoung.domain.DomainTest;
+import javax.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.annotation.Resource;
-import org.springframework.util.Assert;
-
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@DataJpaTest // the test slice
 @Slf4j
 public class TestRepositoryDomainDomainTest {
 
   @Resource
   private DomainTestRepository repository;
 
+  @Resource
+  private TestEntityManager testEntityManager;
+
   @Test
-  public void insertTest() {
+  public void 조회테스트() {
 
+    // given
     var dt = DomainTest.builder().title("test").build();
+    testEntityManager.persistAndFlush(dt);
 
-    var saveDt = repository.save(dt);
+    //when
+    var saveDt = repository.findById(dt.getId()).orElseThrow();
 
-    var isData = repository.existsById(saveDt.getId());
+    //then
+    //assertThat("같은 데이터를 가지고옴",isData,is(true));
 
-    Assert.isTrue(isData, "값이 정확히 들어감");
+    //초기화
+    //repository.delete(saveDt);
 
-    log.debug(saveDt.getCreateDateTime().toString());
+    assertThat(saveDt).as("잘저장됨").isNotNull();
 
-    repository.delete(saveDt);
+    assertThat(saveDt.getTitle()).as("타이틀").isEqualTo("test");
+    assertThat(saveDt.getId()).as("id가 있어야 된다.").isEqualTo(1);
+    assertThat(saveDt.getCreateDateTime()).as("생성일자가 있어야 된다").isNotNull();
+
 
   }
 
